2f99e569aea9dd3b867ae31524a428bb618e4cf3
Alana Nascimento de Souza <74723979+alanazip@users.noreply.github.com>
2022-09-01 14:31:50 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,10 @@
+document.getElementById("Botao").addEventListener("click", function(){
+    let lista = document.createElement("li");
+    let ol = document.createTextNode(ol);
+    lista.appendChild(ol)
+    document.body.appendChild(e);
+    })
+
+    function myFunction(){
+        
+    }
