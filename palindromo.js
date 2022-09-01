811f1a1ddf6d254661109e0f4f4deb6f565349fb
Alana Nascimento de Souza <74723979+alanazip@users.noreply.github.com>
2022-09-01 14:15:52 -0300

    Update palindromo.js

--- palindromo.js
+++ palindromo.js
@@ -1,22 +1,20 @@
-var el = document.getElementById("btVerificar");
-el.addEventListener("click", isPalindromo, false);
-
-function isPalindromo() {
-    let texto = document.getElementById('txtbox').value;
-    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
-    let textInvertido = inverter(textoSemEspaco);
-
-    if (textoSemEspaco == textInvertido)
-        alert(texto + " é palíndromo") 
-    else
-        alert(texto + " não é palíndromo") 
-}
-
 function inverter(str) {
     var newString = "";
     for (var i = str.length - 1; i >= 0; i--) { 
         newString += str[i];
-    }
-    return newString;
-
+	}
+	return newString;
 }
+
+function Palindromo() {
+    let palavra = document.getElementById('campoPalavra').value;
+    let palavraInvertida = inverter(palavra);
+
+    if (palavra == palavraInvertida)
+        alert("A palavra " + palavra + " é um palíndromo")
+    else
+	    alert("A palavra " + palavra + " não é um palíndromo")
+}
+
+var envioPalavra = document.getElementById("envioPalavra");
+envioPalavra.addEventListener("click", Palindromo);
